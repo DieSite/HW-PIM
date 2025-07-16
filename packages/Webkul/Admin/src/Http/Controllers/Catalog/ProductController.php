8384a690280e4de0355f4fa571f834c6586a9797
Luuk <luuk@diesite.nl>
2025-07-16 15:45:48 +0200

    Fix bug with missing product alternatives

--- packages/Webkul/Admin/src/Http/Controllers/Catalog/ProductController.php
+++ packages/Webkul/Admin/src/Http/Controllers/Catalog/ProductController.php
@@ -262,8 +262,6 @@ class ProductController extends Controller
                 Event::dispatch('catalog.product.update.after', $variant);
             }
 
-
-
             session()->flash('success', trans('admin::app.catalog.products.update-success'));
 
             return redirect()->route('admin.catalog.products.edit', [
@@ -468,15 +466,18 @@ class ProductController extends Controller
 
         $size = $product->values['common']['maat'] ?? null;
 
+        // Get other inverse of onderkleed
+
+        $otherRug = $product->parent->variants()->where('values->common->maat', $size)->where('values->common->onderkleed', $otherUnderrug)->first();
+        if (is_null($otherRug)) {
+            return;
+        }
+
         $stockEurogros = $product->values['common']['voorraad_eurogros'] ?? 0;
         $stockDeMunk = $product->values['common']['voorraad_5_korting_handmatig'] ?? 0;
         $stockHW = $product->values['common']['voorraad_hw_5_korting'] ?? 0;
         $stockSale = $product->values['common']['uitverkoop_15_korting'] ?? 0;
 
-        // Get other inverse of onderkleed
-
-        $parent = $product->parent;
-        $otherRug = $parent->variants()->where('values->common->maat', $size)->where('values->common->onderkleed', $otherUnderrug)->first();
         $otherRugValues = $otherRug->values;
         $otherRugValues['common']['voorraad_eurogros'] = $stockEurogros;
         $otherRugValues['common']['voorraad_5_korting_handmatig'] = $stockDeMunk;
