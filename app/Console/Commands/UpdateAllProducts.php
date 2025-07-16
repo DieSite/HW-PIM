399efdea0d4a5f25ea65e801a9dac0e75501bd00
Luuk <luuk@diesite.nl>
2025-07-16 21:48:25 +0200

    Add command to update all products

---
+++ app/Console/Commands/UpdateAllProducts.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Event;
+use Webkul\Product\Models\Product;
+use Webkul\Product\Repositories\ProductRepository;
+
+class UpdateAllProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update-all-products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $productRepository = app()->get(ProductRepository::class);
+        foreach (Product::whereNull('parent_id')->get() as $parent) {
+            $parent = $productRepository->find($parent->id);
+            Event::dispatch('catalog.product.update.after', $parent);
+            foreach ($parent->variants as $variant) {
+                Event::dispatch('catalog.product.update.after', $variant);
+            }
+        }
+    }
+}
