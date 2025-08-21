20b6e7f729d99002245d8956610e084cb5216033
Luuk <luuk@diesite.nl>
2025-08-21 16:27:40 +0200

    Add CalculateMetOnderkleedPrices command

---
+++ app/Console/Commands/CalculateMetOnderkleedPrices.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\ProductService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Event;
+use Webkul\Product\Repositories\ProductRepository;
+
+class CalculateMetOnderkleedPrices extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:calculate-met-onderkleed-prices';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $productRepository = app(ProductRepository::class);
+        $productService = app(ProductService::class);
+
+        $builder = $productRepository->where('values->common->onderkleed', '=', 'Met onderkleed');
+        $count = $builder->count();
+        $this->output->progressStart($count);
+
+        $builder->chunk(100, function ($products) use ($productService) {
+            foreach ($products as $product) {
+                $price = $productService->calculateMetOnderkleedPrice($product);
+                if ($product->values['common']['prijs']['EUR'] != $price) {
+                    $values = $product->values;
+                    $values['common']['prijs']['EUR'] = $price;
+                    $product->values = $values;
+                    $product->save();
+                    Event::dispatch('catalog.product.update.after', $product);
+                }
+                $this->output->progressAdvance();
+            }
+        });
+
+        $this->output->progressFinish();
+    }
+}
