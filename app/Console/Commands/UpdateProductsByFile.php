cbdb3d575e57d394ffde0b7888bda22da466ebdf
Luuk <luuk@diesite.nl>
2025-08-27 13:59:41 +0200

    Add UpdateProductsByFile.php

---
+++ app/Console/Commands/UpdateProductsByFile.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Event;
+use Webkul\Product\Models\Product;
+
+class UpdateProductsByFile extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update-product-by-file';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $filePath = storage_path('app/private/products.csv');
+
+        $file = fopen($filePath, 'r');
+        $skus = [];
+
+        while (($data = fgetcsv($file, 1000, ',')) !== false) {
+            $skus[] = $data[0];
+        }
+
+        $builder = Product::whereIn('sku', $skus);
+
+        $amount = $builder->count();
+        $this->output->progressStart($amount);
+
+        $builder->chunk(100, function ($products) {
+            foreach ($products as $product) {
+                Event::dispatch('catalog.product.update.after', $product);
+
+                foreach ($product->variants as $variant) {
+                    Event::dispatch('catalog.product.update.after', $variant);
+                }
+
+                $this->output->progressAdvance();
+            }
+        });
+
+        $this->output->progressFinish();
+    }
+}
