b1259f3ebeb79b3bc69e4c2166e718ab6fd50d6d
Luuk <luuk@diesite.nl>
2026-04-21 13:53:16 +0200

    Add reporting to job

--- app/Jobs/ImportVoorraadEurogrosJob.php
+++ app/Jobs/ImportVoorraadEurogrosJob.php
@@ -7,9 +7,11 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use RuntimeException;
 use Sentry;
 use Throwable;
 
@@ -17,19 +19,43 @@ class ImportVoorraadEurogrosJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public int $tries = 1;
+
+    public int $timeout = 600;
+
+    public bool $failOnTimeout = true;
+
+    public int $maxExceptions = 1;
+
     protected $path = 'private/eurogros/Voorraad_Eurogros.csv';
+
+    public function middleware(): array
+    {
+        return [(new WithoutOverlapping('import-eurogros-voorraad'))->expireAfter(900)->dontRelease()];
+    }
 
     public function handle(): void
     {
         Log::info('ImportVoorraadEurogrosJob: starting (attempt '.$this->attempts().')');
 
-        if (! $this->pullFromSftp()) {
-            Log::warning('ImportVoorraadEurogrosJob: remote file not found on SFTP, aborting');
-            $this->fail();
-            return;
-        }
+        try {
+            if (! $this->pullFromSftp()) {
+                throw new RuntimeException('Remote file not found on SFTP: /Voorraad/Voorraadlijst/Voorraad_Eurogros.csv');
+            }
 
-        $this->importData();
+            $this->importData();
+        } catch (Throwable $e) {
+            Sentry::withScope(function ($scope) use ($e): void {
+                $scope->setContext('eurogros_import', [
+                    'attempt'     => $this->attempts(),
+                    'remote_path' => '/Voorraad/Voorraadlijst/Voorraad_Eurogros.csv',
+                    'local_path'  => $this->path,
+                ]);
+                Sentry::captureException($e);
+            });
+
+            throw $e;
+        }
 
         Log::info('ImportVoorraadEurogrosJob: completed successfully');
     }
